e6c5483e71537052815dc92acce775c1ef37ee12
Esteban <jesteban@totalware.gr>
2024-09-06 16:51:12 +0300

    Added dfficulty and new alert

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,41 @@
       <h2 class="text-center" draggable="false">Photographic Memory</h2>
     </div>
     <div
+      v-if="!game_start"
       class="d-flex align-items-center justify-content-center"
-      v-if="!processing"
+      style="height: 70vh"
+    >
+      <div class="m-2">
+        <select
+          v-model="selectedDifficulty"
+          class="form-select"
+          style="width: 10vw"
+          aria-label="Default select example"
+        >
+          <option v-for="diff in difficultyOptions" :key="diff" :value="diff">
+            {{ diff.label }}
+          </option>
+        </select>
+      </div>
+      <div class="m-2">
+        <button type="button" class="btn btn-primary" @click="start">
+          Start
+        </button>
+      </div>
+    </div>
+    <div
+      class="d-flex align-items-center justify-content-center"
       draggable="false"
     >
-      <div class="grid-container" draggable="false">
+      <div
+        class="grid-container"
+        :style="{
+          gridTemplateColumns: 'repeat(' + selectedDifficulty.value + ', 1fr)',
+          gridTemplateRows: 'repeat(' + selectedDifficulty.value + ', 1fr)',
+        }"
+        draggable="false"
+        v-show="game_start"
+      >
         <div
           class="grid-item"
           v-for="(card, i) in cards"
@@ -34,6 +64,7 @@
                   draggable="false"
                 >
                   <img
+                    loading="eager"
                     :src="getCharIcon(card.num)"
                     style="
                       width: 100%;
@@ -52,47 +83,82 @@
         </div>
       </div>
     </div>
-    <div class="d-flex justify-content-center">
-      <div>
-        <h5>Attempts: {{ tries }}</h5>
-      </div>
-    </div>
-    <div class="d-flex justify-content-center">
-      <div>
-        <button type="button" class="btn btn-primary" @click="reset">
-          Reset
-        </button>
-      </div>
-    </div>
-    <strong class="ms-5">
-      made by
-      <a
-        href="https://github.com/JasonEst-11/Photographic-Memory"
-        target="_blank"
-        >JasonEst-11</a
-      >
-    </strong>
   </div>
+
+  <strong class="signature">
+    made by
+    <a href="https://github.com/JasonEst-11/Photographic-Memory" target="_blank"
+      >JasonEst-11</a
+    >
+  </strong>
 </template>
 
 <script>
 export default {
   data() {
     return {
+      difficultyOptions: [
+        {
+          label: "Easy",
+          value: 2,
+          time: 0.5,
+        },
+        {
+          label: "Medium",
+          value: 4,
+          time: 3.5,
+        },
+        {
+          label: "Hard",
+          value: 6,
+          time: 6,
+        },
+      ],
+      selectedDifficulty: {
+        label: "Easy",
+        value: 2,
+        time: 0.5,
+      },
       cards: [],
       selected: [],
       tries: 0,
       char_map: [],
-      loading: false,
+      game_start: false,
     };
   },
-
+  computed: {
+    difficultyPairs() {
+      let grid = this.selectedDifficulty.value;
+      return (grid * grid) / 2;
+    },
+  },
   methods: {
+    initData() {
+      this.cards = [];
+      this.char_map = [];
+      for (let i = 1; i <= this.difficultyPairs; i++) {
+        //Add to cards
+        this.cards.push({ num: i, show: false });
+        this.cards.push({ num: i, show: false });
+        setTimeout(() => {
+          let url =
+            "https://picsum.photos/200?random=" +
+            new Date().getTime() +
+            i +
+            ".webp";
+          //Add to char_map
+          this.char_map.push({
+            id: i,
+            icon: url,
+          });
+        }, 1);
+      }
+    },
     shuffle() {
       this.tries = 0;
       this.selected = [];
-      let currentIndex = this.cards.length,
-        randomIndex;
+      let currentIndex = this.cards.length;
+      let randomIndex;
       while (currentIndex != 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
         currentIndex--;
@@ -102,9 +168,6 @@ export default {
         ];
       }
       this.cards.forEach((el) => (el.show = false));
-    },
-    getCharIcon(id) {
-      return this.char_map.find((el) => el.id === id).icon;
     },
     flip(event, card) {
       if (this.selected.length != 2) {
@@ -130,39 +193,89 @@ export default {
               });
               this.selected = [];
             }
-          }, 700);
+          }, 500);
         }
       }
     },
-    initData() {
-      this.cards = [];
-      this.char_map = [];
-      for (let i = 1; i <= 8; i++) {
-        //Add to cards
-        this.cards.push({ num: i, show: false });
-        this.cards.push({ num: i, show: false });
-        let url = "https://picsum.photos/200/200?random=" + i;
-        //Add to char_map
-        this.char_map.push({
-          id: i,
-          icon: url,
-        });
+    getCharIcon(id) {
+      let char = this.char_map.find((el) => el.id === id);
+      if (char) {
+        return char.icon;
       }
     },
     checkIfWin() {
       let res = this.cards.filter((el) => el.show === false);
       if (res.length === 0) {
-        alert("Well done!");
-        this.reset();
+        if (
+          this.tries <= this.difficultyPairs &&
+          this.selectedDifficulty.value === 2
+        ) {
+          this.$swal
+            .fire({
+              title: "Not even a challenge huh? 😎",
+              showCancelButton: true,
+              confirmButtonText: "Replay",
+            })
+            .then((result) => {
+              if (result.isConfirmed) {
+                this.start();
+              } else {
+                this.game_start = false;
+              }
+            });
+        } else if (this.tries <= this.difficultyPairs) {
+          this.$swal
+            .fire({
+              title: "Perfect!!! Did you cheat? 🧐",
+              showCancelButton: true,
+              confirmButtonText: "Replay",
+            })
+            .then((result) => {
+              if (result.isConfirmed) {
+                this.start();
+              } else {
+                this.game_start = false;
+              }
+            });
+        } else {
+          this.$swal
+            .fire({
+              title: "Well done",
+              text: "Total attempts: " + this.tries,
+              showCancelButton: true,
+              confirmButtonText: "Replay",
+            })
+            .then((result) => {
+              if (result.isConfirmed) {
+                this.start();
+              } else {
+                this.game_start = false;
+              }
+            });
+        }
       }
     },
-    reset() {
-      window.location.reload();
+    start() {
+      this.game_start = true;
+      this.initData();
+      this.shuffle();
+      this.cards.forEach((el) => (el.show = true));
+      setTimeout(() => {
+        this.cards.forEach((el) => (el.show = false));
+      }, 1000 * this.selectedDifficulty.time);
     },
   },
   mounted() {
     this.initData();
     this.shuffle();
+  },
+  watch: {
+    selectedDifficulty(newVal, oldVal) {
+      if (newVal.value != oldVal.value) {
+        this.initData();
+        this.shuffle();
+      }
+    },
   },
 };
 </script>
@@ -184,8 +297,8 @@ body {
 }
 .grid-container {
   display: grid;
-  grid-template-columns: repeat(4, 1fr);
-  grid-template-rows: repeat(4, 1fr);
+  /* grid-template-columns: repeat(4, 1fr);
+  grid-template-rows: repeat(4, 1fr); */
   width: 80vmin;
   height: 80vmin;
   gap: 10px;
@@ -256,5 +369,11 @@ body {
   display: flex;
   align-items: center;
   justify-content: center;
+}
+
+.signature {
+  position: fixed;
+  left: 3rem;
+  bottom: 1rem;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,4 +2,6 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.css'
-createApp(App).mount('#app')
+import VueSweetalert2 from 'vue-sweetalert2';
+import 'sweetalert2/dist/sweetalert2.min.css';
+createApp(App).use(VueSweetalert2).mount('#app')
